87487bbaae6a9dc440d4f1d2d85ba7c1ce89eaff
518997 <160498566+518997@users.noreply.github.com>
2024-02-20 09:44:17 +0100

    Add files via upload

---
+++ naam.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triple Crown</title>
+</head>
+<body>
+    <h1>Dankjewel!</h1>
+
+    <p>
+      <? 
+      echo 'Bedankt voor het invullen van je naam, ' . $_GET['gebruikersnaam'] . '!';
+      
+      
+      ?>
+    </p>
+
+
+    </form>
+</body>
+</html>
